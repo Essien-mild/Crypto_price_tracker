7a372c7811cc1ae294d03f41481b0d7693101641
Essien <essienetim4002@gmail.com>
2025-09-25 17:58:55 +0200

    3rd commit

--- crypto_tracker/settings.py
+++ crypto_tracker/settings.py
@@ -19,6 +19,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -134,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, "static"),
 ]
