13c3f9f29b6d0c716cf59ca5ee4a088d5ae180bd
Essien <essienetim4002@gmail.com>
2025-09-25 16:15:56 +0200

    second commit

--- crypto_tracker/settings.py
+++ crypto_tracker/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
+ALLOWED_HOSTS = ['mild.pythonanywhere.com']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
